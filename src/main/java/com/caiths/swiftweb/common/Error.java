4e1731bc4489ee8f5bc8cd0ea1df9444d693a1bc
poboll <caiths@icloud.com>
2024-11-19 14:09:36 +0800

    feat: Refactor error handling and update project configurations
    - Updated project version in `pom.xml` from `0.0.2` to `0.0.3`.
    - Modified `Error.java` interface:
      - Removed redundant comments and unnecessary method definitions.
      - Simplified error code structure for consistency and readability.
    - Refined `ResultUtils.java`:
      - Refactored error handling methods to be more generic and reusable.
      - Updated `error` methods to return `BaseResponse<T>` instead of `BaseResponse`.
      - Improved error code and message handling to be more flexible and consistent across various response types.
    - Updated `Knife4jConfig.java`:
      - Cleaned up redundant field initializations.
      - Refined API description, contact info, and versioning for better clarity.
      - Ensured that the Swagger configuration for API documentation includes the proper metadata.

--- src/main/java/com/caiths/swiftweb/common/Error.java
+++ src/main/java/com/caiths/swiftweb/common/Error.java
@@ -8,20 +8,18 @@ package com.caiths.swiftweb.common;
  * @description: 错误返回接口, 自定义错误枚举实现该接口即可
  */
 public interface Error {
+
     /**
      * 错误代码
      *
      * @return int
      */
     int getCode();
+
     /**
      * 错误描述
      *
      * @return {@link String}
      */
     String getMessage();
-    /**
-     * 系统错误 ()
-     */
-    int SYSTEM_ERROR = 50000;
 }
